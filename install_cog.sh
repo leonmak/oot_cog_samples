5b3ff97f5355b0ab36e91d8f9859d594703d4e4b
Leon Mak <leonmakansheng@gmail.com>
2024-02-28 18:09:52 +0800

    install screen

--- install_cog.sh
+++ install_cog.sh
@@ -2,3 +2,5 @@
 
 sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"
 sudo chmod +x /usr/local/bin/cog
+
+sudo apt-get install screen
